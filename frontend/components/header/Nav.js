50c22e51abfb684cb14b8c2a33103dd6c7d6e53d
RajRajhans <me@rajrajhans.com>
2021-07-11 22:28:53 +0530

    show hamburger icon on mobiles and not on desktop which opens popup animated by framer motion

--- frontend/components/header/Nav.js
+++ frontend/components/header/Nav.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+import useAnimatedNavToggler from '../../utils/useAnimatedNavToggler';
+import MenuIcon from '../../public/menu-icon.svg';
+import Cart from './Cart';
+import { motion } from 'framer-motion';
 
 const navItems = [
   {
@@ -28,6 +32,9 @@ const navItems = [
 const NavContainer = styled.div`
   display: flex;
   justify-content: center;
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavItemContainer = styled.div`
@@ -47,6 +54,40 @@ const NavItemContainer = styled.div`
   }
 `;
 
+const NavToggler = styled.button`
+  z-index: 20;
+  transition: all 0.3s;
+
+  :hover {
+    color: var(--accent);
+  }
+
+  :focus {
+    outline: none;
+    font-weight: 500;
+  }
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileNavLinks = motion(styled.div`
+  z-index: 10;
+  position: fixed;
+  top: 0;
+  inset: 0;
+  margin: 4px 6px;
+  padding: 10px;
+  border: 1px solid var(--black);
+  border-radius: 10%;
+  background: white;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`);
+
 const NavItem = ({ navItem }) => (
   <NavItemContainer>
     <Link href={navItem.route}>
@@ -56,12 +97,35 @@ const NavItem = ({ navItem }) => (
 );
 
 const Nav = () => {
+  const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
   return (
-    <NavContainer>
-      {navItems.map((navItem) => (
-        <NavItem key={navItem.id} navItem={navItem} />
-      ))}
-    </NavContainer>
+    <>
+      <NavContainer>
+        {navItems.map((navItem) => (
+          <NavItem key={navItem.id} navItem={navItem} />
+        ))}
+      </NavContainer>
+
+      <Cart />
+
+      <NavToggler
+        onClick={toggleNavbar}
+        className={showNavLinks ? 'open' : 'closed'}
+      >
+        {!showNavLinks && (
+          <>
+            <MenuIcon style={{ width: '20px', height: '20px' }} />
+          </>
+        )}
+      </NavToggler>
+
+      <MobileNavLinks
+        initial={{ x: '150%', display: 'none' }}
+        animate={animation}
+      >
+        Nav links and close box
+      </MobileNavLinks>
+    </>
   );
 };
 
