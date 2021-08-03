72932b336bc189a382f6e9ba1d37685a33f6acd6
RajRajhans <me@rajrajhans.com>
2021-08-03 16:53:01 +0530

    update _app to hook in nprogress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import NProgress from 'nprogress';
+import '../styles/nprogress.css';
+import Router from 'next/router';
 import PageLayout from '../components/PageLayout';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({ Component, pageProps }) => {
   return (
