6c4f9dfbd452d399a1ffec256956bfd797f018d6
RajRajhans <me@rajrajhans.com>
2021-08-17 14:08:23 +0530

    add global styles

--- frontend/components/PageLayout.js
+++ frontend/components/PageLayout.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import Header from './header/Header';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Mulish', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family: 'Mulish', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
 
 const PageLayout = ({ children }) => {
   return (
     <>
-      <Header />
-      {children}
+      <GlobalStyles />
+      <InnerStyles>
+        <Header />
+        {children}
+      </InnerStyles>
     </>
   );
 };
--- frontend/components/header/Header.js
+++ frontend/components/header/Header.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Logo from './Logo';
 import Nav from './Nav';
 import Cart from './Cart';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 0;
+`;
 
 const Header = () => {
   return (
-    <>
+    <HeaderContainer>
       <Logo />
       <Nav />
       <Cart />
-    </>
+    </HeaderContainer>
   );
 };
 
