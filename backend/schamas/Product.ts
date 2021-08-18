de9c2c26e55e9da442a28f7644e30d7599761f16
RajRajhans <me@rajrajhans.com>
2021-08-18 18:01:40 +0530

    add relationship between Product andProductImage data types in keystone

--- backend/schamas/Product.ts
+++ backend/schamas/Product.ts
@@ -1,11 +1,20 @@
 import { list } from '@keystone-next/keystone/schema';
-import { integer, select, text } from '@keystone-next/fields';
+import { integer, relationship, select, text } from '@keystone-next/fields';
 
 export const Product = list({
   // access:
   fields: {
     name: text({ isRequired: true }),
     description: text({ ui: { displayMode: 'textarea' } }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
     status: select({
       options: [
         { label: 'Draft', value: 'DRAFT' },
@@ -16,6 +25,5 @@ export const Product = list({
       ui: { displayMode: 'segmented-control' },
     }),
     price: integer(),
-    // TODO: Photo
   },
 });
--- backend/schamas/ProductImage.ts
+++ backend/schamas/ProductImage.ts
@@ -1,6 +1,6 @@
 import { list } from '@keystone-next/keystone/schema';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
-import { text } from '@keystone-next/fields';
+import { relationship, text } from '@keystone-next/fields';
 import 'dotenv/config';
 
 export const cloudinary = {
@@ -14,5 +14,9 @@ export const ProductImage = list({
   fields: {
     image: cloudinaryImage({ cloudinary, label: 'Source' }),
     altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: { initialColumns: ['image', 'altText', 'product'] },
   },
 });
