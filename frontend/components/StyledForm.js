f1c3996013b19fbec8fa116eb08ce1e154e5a679
RajRajhans <me@rajrajhans.com>
2021-08-23 21:53:58 +0530

    update styledForm styles for textarea and fixing input fonts

--- frontend/components/StyledForm.js
+++ frontend/components/StyledForm.js
@@ -12,7 +12,8 @@ const StyledForm = styled.form`
     display: block;
   }
 
-  input {
+  input,
+  textarea {
     font-size: 16px;
     line-height: 28px;
     padding: 8px 16px;
@@ -25,9 +26,13 @@ const StyledForm = styled.form`
       rgba(0, 0, 0, 0) 0 0 0 0, rgba(60, 66, 87, 0.16) 0 0 0 1px,
       rgba(0, 0, 0, 0) 0 0 0 0, rgba(0, 0, 0, 0) 0 0 0 0,
       rgba(0, 0, 0, 0) 0 0 0 0;
+    font-family: 'Mulish', --apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+      sans-serif;
   }
 
-  input:focus {
+  input:focus,
+  textarea:focus {
     border: none;
     border-radius: 2pt;
     box-shadow: 0 0 0 1pt var(--accent);
@@ -36,14 +41,37 @@ const StyledForm = styled.form`
   }
 
   input[type='submit'] {
-    background-color: rgb(84, 105, 212);
+    background-color: var(--dark);
     box-shadow: rgba(0, 0, 0, 0) 0 0 0 0, rgba(0, 0, 0, 0) 0 0 0 0,
-      rgba(0, 0, 0, 0.12) 0 1px 1px 0, rgb(84, 105, 212) 0 0 0 1px,
+      rgba(0, 0, 0, 0.12) 0 1px 1px 0, var(--accent) 0 0 0 1px,
       rgba(0, 0, 0, 0) 0 0 0 0, rgba(0, 0, 0, 0) 0 0 0 0,
       rgba(60, 66, 87, 0.08) 0 2px 5px 0;
     color: #fff;
-    font-weight: 600;
+    font-weight: 700;
     cursor: pointer;
+    transition: all 0.2s ease;
+    font-family: 'Mulish', --apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+      sans-serif;
+  }
+
+  input[type='submit']:hover {
+    background-color: var(--accent);
+  }
+
+  input[type='submit']:focus {
+    background-color: var(--dark);
+  }
+
+  textarea {
+    width: 100%;
+    padding: 0.5rem;
+    font-size: 1rem;
+  }
+
+  fieldset {
+    border: 0;
+    padding: 0;
   }
 `;
 
