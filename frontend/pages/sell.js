950993a36c87f524a001c582bcb425bf80b7fea7
RajRajhans <me@rajrajhans.com>
2021-08-23 21:54:36 +0530

    update sell page to include create product form

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import CreateProductForm from '../components/CreateProductForm';
+import CardBg from '../components/CardBg';
+import PageInfoBar from '../components/PageInfoBar';
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  background: var(--light);
+  padding: 25px;
+  border-radius: 25px;
+  box-shadow: 0 2px 2px 0 rgba(31, 38, 135, 0.37);
+  margin: 40px 50px;
+`;
+
+const InnerFormContainer = styled.div`
+  width: 50%;
+  margin: auto;
+`;
 
 const Sell = () => {
-  return <div>SELL</div>;
+  return (
+    <div>
+      <CardBg />
+      <PageInfoBar
+        leftText={'Add New Products'}
+        middleText={'Sell'}
+        rightComponent={null}
+      />
+      <FormContainer>
+        <InnerFormContainer>
+          <CreateProductForm key={'CreateProductForm'} />
+        </InnerFormContainer>
+      </FormContainer>
+    </div>
+  );
 };
 
 export default Sell;
