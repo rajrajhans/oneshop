fece0e1342f14dd5e39f6ee5f7ac42291e227298
RajRajhans <me@rajrajhans.com>
2021-08-23 15:50:22 +0530

    add useForm hook util

---
+++ frontend/utils/useForm.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const useForm = (initialState = {}) => {
+  const [inputState, setInputState] = useState(initialState);
+
+  const handleChange = (e) => {
+    let { value, name, type } = e.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      value = e.target.files[0];
+    }
+
+    setInputState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const resetForm = () => {
+    setInputState(initialState);
+  };
+
+  const clearForm = () => {
+    const blankState = Object.fromEntries(
+      Object.entries(inputState).map(([key, value]) => [key, '']),
+    );
+    setInputState(blankState);
+  };
+
+  return [inputState, handleChange, resetForm, clearForm];
+};
