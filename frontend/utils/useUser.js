26d5010723c2ceef6af70645bb70b8b02fe4c9c4
RajRajhans <me@rajrajhans.com>
2021-08-27 13:35:33 +0530

    add a custom hook for getting currently logged in user details

---
+++ frontend/utils/useUser.js
@@ -0,0 +1,21 @@
+// custom hook for getting the current logged in user's details
+
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+export function useUser() {
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+}
+
+const CURRENT_USER_QUERY = gql`
+  query {
+    authenticatedItem {
+      ... on User {
+        id
+        email
+        name
+      }
+    }
+  }
+`;
