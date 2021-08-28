0fca094131223aeeaf5d63010c74bcbbeabff9d1
RajRajhans <me@rajrajhans.com>
2021-08-28 21:03:12 +0530

    update CartState to include cart count as well

--- frontend/components/cart/CartState.js
+++ frontend/components/cart/CartState.js
@@ -1,9 +1,20 @@
 import { useState, createContext, useContext } from 'react';
+import useCartCount from './useCartCount';
 
 export const CartState = createContext(null);
 
 export const CartStateProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const { getCartCount } = useCartCount();
+  const [cartCount, setCartCount] = useState(getCartCount());
+
+  const incrementCartCount = () => {
+    setCartCount(cartCount + 1);
+  };
+
+  const decrementCartCount = () => {
+    setCartCount(cartCount - 1);
+  };
 
   const openCart = () => {
     setIsCartOpen(true);
@@ -14,7 +25,16 @@ export const CartStateProvider = ({ children }) => {
   };
 
   return (
-    <CartState.Provider value={{ isCartOpen, openCart, closeCart }}>
+    <CartState.Provider
+      value={{
+        isCartOpen,
+        openCart,
+        closeCart,
+        cartCount,
+        incrementCartCount,
+        decrementCartCount,
+      }}
+    >
       {children}
     </CartState.Provider>
   );
