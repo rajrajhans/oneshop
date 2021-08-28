1b8358254e19668fca45ec567bc92610ab99474c
RajRajhans <me@rajrajhans.com>
2021-08-28 22:00:06 +0530

    update remove from cart

--- frontend/components/cart/useRemoveFromCart.js
+++ frontend/components/cart/useRemoveFromCart.js
@@ -1,12 +1,16 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
 
+const update = (cache, payload) => {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+};
+
 export default function useRemoveFromCart(id) {
   const [deleteCartItem, { error, loading }] = useMutation(DELETE_CART_ITEM, {
     variables: {
       id: id,
     },
-    refetchQueries: ['CURRENT_USER_QUERY'],
+    update,
   });
 
   return { deleteCartItem, error, loading };
