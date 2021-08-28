e0989a1b32eca639e21d49de076a39fdb73b68bd
RajRajhans <me@rajrajhans.com>
2021-08-28 12:47:21 +0530

    send user to sign in page once they have signed out

--- frontend/components/auth/SignOut.js
+++ frontend/components/auth/SignOut.js
@@ -3,14 +3,19 @@ import SignOutIcon from '../../public/assets/log-out-icon.svg';
 import IconWithTooltip from '../IconWithTooltip';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
 
 const SignOut = () => {
+  const router = useRouter();
   const [signOut] = useMutation(SIGN_OUT_MUTATION, {
     refetchQueries: ['CURRENT_USER_QUERY'],
   });
 
   const handleClick = () => {
     signOut().catch((e) => {
+      console.log(e);
+    });
+    router.push('/sign-in').catch((e) => {
       console.log(e);
     });
   };
