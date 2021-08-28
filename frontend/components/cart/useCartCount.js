7212d827523e482c5172caa9fb5d2b0e9863af32
RajRajhans <me@rajrajhans.com>
2021-08-28 21:03:38 +0530

    update useCartCount to get cart directly from gql query using useUser hook

--- frontend/components/cart/useCartCount.js
+++ frontend/components/cart/useCartCount.js
@@ -1,6 +1,10 @@
-export default function useCartCount(cart) {
+import { useUser } from '../../utils/useUser';
+
+export default function useCartCount() {
+  const currentUser = useUser();
+
   const getCartCount = () => {
-    return cart.reduce((acc, cartItem) => {
+    return currentUser.cart.reduce((acc, cartItem) => {
       return acc + cartItem.quantity;
     }, 0);
   };
