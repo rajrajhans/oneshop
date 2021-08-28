d12337991a6a44368d31be77171a45167c526081
RajRajhans <me@rajrajhans.com>
2021-08-28 16:19:06 +0530

    remove console logs

--- frontend/utils/calculateTotalCartPrice.js
+++ frontend/utils/calculateTotalCartPrice.js
@@ -1,6 +1,5 @@
 export default function calculateTotalCartPrice(cart) {
   return cart.reduce((tally, cartItem) => {
-    console.log({ tally, cartItem });
     if (!cartItem.product) {
       return tally; //because products can be deleted from inventory, but they can still be in someone's cart!
     }
