1a0c54a13c3450b90a66c62aa07d2fab7a975316
RajRajhans <me@rajrajhans.com>
2021-09-24 13:50:45 +0530

    handle errors in stripe Checkout

--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, Elements } from '@stripe/react-stripe-js';
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
 import { ProductButton } from './ProductCard';
+import { useLoadingContext } from './LoadingContext';
+import nProgress from 'nprogress';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -26,20 +33,46 @@ const CheckoutFormStyles = styled.form`
 
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
+const CheckoutContainer = () => (
+  <Elements stripe={stripeLib}>
+    <Checkout />
+  </Elements>
+);
+
 const Checkout = () => {
-  const handleSubmit = (e) => {
+  const { toggleIsLoading } = useLoadingContext();
+  const [error, setError] = useState('');
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    // 1. start showing loading indicator & start page transition
+    // 2. create payment method via stripe (token comes back if this is successful)
+    // 3. handle any errors from stripe
+    // 4. send the stripe token to keystone server, via a custom mutation
+    // 5. change the page to "view order"
+    // 6. close cart
+    // 7. stop showing loading indicator
     e.preventDefault();
-    alert('yo');
+    toggleIsLoading(true);
+    nProgress.start();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    if (error) {
+      setError(error);
+    }
+    toggleIsLoading(false);
   };
 
   return (
-    <Elements stripe={stripeLib}>
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        <CardElement />
-        <ProductButton type={'submit'}>Check Out</ProductButton>
-      </CheckoutFormStyles>
-    </Elements>
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+      <CardElement />
+      <ProductButton type={'submit'}>Check Out</ProductButton>
+    </CheckoutFormStyles>
   );
 };
 
-export default Checkout;
+export default CheckoutContainer;
