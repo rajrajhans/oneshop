ae80a410521ef5ac5df25ff505776e03179c287d
RajRajhans <me@rajrajhans.com>
2021-09-25 22:06:24 +0530

    tighten access for CRUD-ing roles

--- backend/schemas/Role.ts
+++ backend/schemas/Role.ts
@@ -1,8 +1,20 @@
 import { list } from '@keystone-next/keystone/schema';
 import { relationship, text } from '@keystone-next/fields';
 import { permissionFields } from './fields';
+import { permissions } from '../access';
 
 export const Role = list({
+  access: {
+    create: permissions.canManageRoles,
+    read: permissions.canManageRoles,
+    update: permissions.canManageRoles,
+    delete: permissions.canManageRoles,
+  },
+  ui: {
+    hideCreate: (args) => !permissions.canManageRoles,
+    hideDelete: (args) => !permissions.canManageRoles,
+    isHidden: (args) => !permissions.canManageRoles,
+  },
   fields: {
     name: text({ isRequired: true }),
     ...permissionFields,
