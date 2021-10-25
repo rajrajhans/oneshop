0c69e0d2050e841ff465bb570f2da8effffdf192
RajRajhans <me@rajrajhans.com>
2021-10-25 12:53:48 +0530

    hotfix/fix-network-error-in-production

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 export const devEndpoint =
-  'http://mysterious-garden-13821.herokuapp.com/api/graphql';
+  'https://mysterious-garden-13821.herokuapp.com/api/graphql';
 export const prodEndpoint =
-  'http://mysterious-garden-13821.herokuapp.com/api/graphql';
+  'https://mysterious-garden-13821.herokuapp.com/api/graphql';
 export const ProductsPerPage = 9;
